85b2b32964f134d0d9f7b1ea8b46728c6eeed51e
1ven <1ven.mironchik@gmail.com>
2017-09-14 14:31:34 +0300

    Replace arrow function

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var chunks = require('chunks');
+var chunks = require("chunks");
 
-module.exports = chunks.methods("GET", req =>
-  chunks.json(Promise.resolve([1, 2, 3]))
-);
+module.exports = chunks.methods("GET", function(req) {
+  return chunks.json(Promise.resolve([1, 2, 3]));
+});
